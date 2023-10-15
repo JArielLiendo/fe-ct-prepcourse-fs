417ce560e4066e7be6a2aea6626be8be7344ed4a
JALiendo <jjaaliendo@gmail.com>
2023-10-14 23:43:13 -0300

    Modulo 08

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,22 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+
+   return nombre[0].toUpperCase() + nombre.slice(1);
+
+   /*var conMay = nombre.split('');
+
+   conMay[0] = conMay[0].toUpperCase();
+   
+   return conMay.join('');*/
+   
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +28,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+    return cb (num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,6 +37,17 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+
+   /*var suma = 0;
+   arrayOfNumbers.map((elemento) => {
+      suma += elemento;
+   });*/
+
+   var suma =arrayOfNumbers.reduce(function(acumulador, elemento){
+      return acumulador + elemento;
+   });
+
+    cb(suma);
 }
 
 function forEach(array, cb) {
@@ -33,6 +55,10 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   
+   array.forEach((elem)=>{
+      cb(elem);
+   });
 }
 
 function map(array, cb) {
@@ -40,12 +66,44 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+
+  /* nuevoArray = [];
+   array.map((elem)=>{
+      return nuevoArray.push(cb(elem));
+   });*/
+
+   var nuevoArray = array.map(function(elem){
+      return cb(elem);
+   });
+
+   return nuevoArray;
 }
+
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+
+   var nuevoArray = [];
+
+  /* for (i=0; i<arrayOfStrings.length; i++){
+      if(arrayOfStrings[i][0]==="a" || arrayOfStrings[i][0]==="A"){
+         nuevoArray.push(arrayOfStrings[i]);
+      }
+   };*/
+   
+   nuevoArray = arrayOfStrings.filter((elem)=>{//Metodo "filter"
+      return elem[0] === "a";
+   });
+
+  /* arrayOfStrings.map((elem)=>{
+      if (elem[0]=== "a" || elem[0]==="A"){
+         return nuevoArray.push(elem);
+      }
+    });*/
+   
+   return nuevoArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
